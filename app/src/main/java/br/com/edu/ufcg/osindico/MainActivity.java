d9c3455cf273e245b22e70d22d463481afc30c19
Antonio Lúcio <antoniolucio_@hotmail.com>
2017-02-20 22:12:31 -0300

    Rabisco tela login

--- app/src/main/java/br/com/edu/ufcg/osindico/MainActivity.java
+++ app/src/main/java/br/com/edu/ufcg/osindico/MainActivity.java
@@ -2,12 +2,25 @@ package br.com.edu.ufcg.osindico;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    View main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        main = findViewById(R.id.activity_main);
+        main.setBackgroundColor(getResources().getColor(android.R.color.white));
+
+
+
+
     }
 }
+
+
