5d3f5611bc72d3b91692e23922d6bd569f9d3912
pablo <pablo.santos@ccc.ufcg.edu.br>
2017-03-04 21:36:14 -0300

    Implementação do tela de navegação do morador

--- app/src/main/java/br/com/edu/ufcg/osindico/navigationDweller/NavigationDwellerActivity.java
+++ app/src/main/java/br/com/edu/ufcg/osindico/navigationDweller/NavigationDwellerActivity.java
@@ -7,7 +7,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
 import br.com.edu.ufcg.osindico.R;
@@ -17,9 +16,9 @@ import butterknife.ButterKnife;
 public class NavigationDwellerActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    @BindView(R.id.toolbar) Toolbar toolbar;
-    @BindView(R.id.drawer_layout) DrawerLayout drawer;
-    @BindView(R.id.nav_view) NavigationView navigationView;
+    @BindView(R.id.toolbarDweller) Toolbar toolbar;
+    @BindView(R.id.drawer_layout_dweller) DrawerLayout drawer;
+    @BindView(R.id.nav_view_dweller) NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,47 +46,33 @@ public class NavigationDwellerActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.navigation_dweller, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.nav_home) {
+        switch (id) {
+            case R.id.nav_home:
 
-        } else if (id == R.id.nav_calendar) {
+                break;
+            case R.id.nav_calendar_dweller:
 
-        } else if (id == R.id.nav_messages) {
+                break;
+            case R.id.nav_messages_dweller:
 
-        } else if (id == R.id.nav_my_condo) {
+                break;
+            case R.id.nav_my_condo_dweller:
 
-        } else if (id == R.id.nav_settings) {
+                break;
+            case R.id.nav_settings_dweller:
 
-        } else if (id == R.id.nav_about) {
+                break;
+            case R.id.nav_about_dweller:
 
-        } else if (id == R.id.nav_logout) {
+                break;
+            case R.id.nav_logout:
 
+                break;
         }
 
         drawer.closeDrawer(GravityCompat.START);
