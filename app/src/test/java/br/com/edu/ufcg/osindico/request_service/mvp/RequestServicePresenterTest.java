2513eb1e31ad0310f3292b147521167db3f2f518
ebarross <2974825e>
2017-03-20 12:38:25 -0300

    Atualização da versão do mockito.

--- app/src/test/java/br/com/edu/ufcg/osindico/request_service/mvp/RequestServicePresenterTest.java
+++ app/src/test/java/br/com/edu/ufcg/osindico/request_service/mvp/RequestServicePresenterTest.java
@@ -5,12 +5,6 @@ import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import br.com.edu.ufcg.osindico.request_service.mvp.RequestServicePresenterImpl;
-
-/**
- * Created by emanoel on 16/03/17.
- */
-
 public class RequestServicePresenterTest {
 
     private RequestServicePresenterImpl presenter;
